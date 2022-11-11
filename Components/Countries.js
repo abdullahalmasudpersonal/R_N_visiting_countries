bc452f9d648c19f4423356b450de58ac5884216a
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2022-11-11 22:38:23 +0600

    load rest countries

---
+++ Components/Countries.js
@@ -0,0 +1,23 @@
+import { View, Text, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import Country from './Country';
+
+export default function Countries() {
+    const [countries, setCountries] = useState([]);
+    useEffect( () =>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data));
+    }, [])
+
+  return (
+    <View>
+      <Text>Countries: {countries.length}</Text>
+      <ScrollView>
+        {
+            countries.map(country => <Country country={country} />)
+        }
+      </ScrollView>
+    </View>
+  )
+}
